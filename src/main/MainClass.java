a7ee524a1bfd8a93b8bf86c77fda861a636ae50d
jeongwoo <dahlia11444@gmail.com>
2021-03-02 19:42:32 +0900

    기본프로잭트생성

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,5 +1,26 @@
 package main;
 
-public class MainClass {
+import java.util.Scanner;
 
+public class MainClass {
+	public static void main(String[] args) {
+		//kim - 로그인
+		//cho - 회원관리
+		Scanner sc = new Scanner(System.in);
+		int num;
+		
+		while(true) {
+			System.out.println("1.로그인 2.회원관리");
+			num = sc.nextInt();
+			switch (num) {
+			case 1:
+				break;
+			case 2:
+				break;
+				
+				
+			}
+			
+		}
+	}
 }
