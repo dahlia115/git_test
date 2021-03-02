272699510c8542014386fa0f8b06b72d03be2582
jeongwoo <dahlia11444@gmail.com>
2021-03-02 20:07:17 +0900

    로그인 기능 구현

---
+++ src/login/Login.java
@@ -0,0 +1,7 @@
+package login;
+
+public class Login {
+	public void login() {
+		System.out.println("로그인기능 구현");
+	}
+}
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,5 +1,30 @@
 package main;
 
-public class MainClass {
+import java.util.Scanner;
 
+import login.Login;
+
+public class MainClass {
+	public static void main(String[] args) {
+		//kim - 로그인
+		//cho - 회원관리
+		Scanner sc = new Scanner(System.in);
+		int num;
+		
+		while(true) {
+			System.out.println("1.로그인 2.회원관리");
+			num = sc.nextInt();
+			switch (num) {
+			case 1:
+				Login l = new Login();
+				l.login();
+				break;
+			case 2:
+				break;
+				
+				
+			}
+			
+		}
+	}
 }
